d7554c6bf0e50669af1221dcc535dcf2438d5944
TJ Miller <tj@themillers.co>
2026-02-19 08:45:40 -0500

    Update to v1.10

--- api/client.go
+++ api/client.go
@@ -14,7 +14,7 @@ import (
 	"time"
 )
 
-var apiVersion = "v1.9"
+var apiVersion = "v1.10"
 
 func Request(method string, path string, c *cli.Context) ([]byte, bool, error) {
 	hostname := c.String("hostname")
